a551c822d65c5018aaf9beee2a2bcc525e5a6486
Andrew Brennwald & Jessica Joly <student@epicodus.com>
2015-09-16 16:44:12 -0700

    Implement Game object, player X and player O turn functions, and framework for victory function.

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,9 @@ function Board() {
   this.spaces = [];
 };
 
+// Build a new space object for each x and y coordinate combo,
+// then push them into the Board's spaces array as they're made.
+
 Board.prototype.buildBoard = function() {
   for (var y = 1; y < 4; y++) {
     for (var x = 1; x < 4; x++) {
@@ -20,3 +23,48 @@ Board.prototype.buildBoard = function() {
     };
   };
 };
+
+// Creates a game with two instances of Player and an instance of Board
+
+function Game(playerX, playerO, board) {
+  this.playerX = playerX;
+  this.playerO = playerO;
+  this.board = board;
+};
+
+// Takes a turn for player X by marking the clicked spot with X and asking
+// if victory was obtained
+
+Game.prototype.playerXTurn = function(space) {
+  this.space = space;
+  this.board.spaces[space].mark = "X";
+  this.victory(space);
+}
+
+// Takes a turn for player O by marking the clicked spot with X and asking
+// if victory was obtained
+
+Game.prototype.playerOTurn = function(space) {
+  this.space = space;
+  this.board.spaces[space].mark = "O";
+  this.victory(space);
+}
+
+// Determining whether or not horizontal, vertical, or diagonal victory was
+// obtained based on a changed space.
+
+Game.prototype.victory = function(clicked) {
+  // What space was clicked?
+  this.clicked = clicked;
+
+  // spaces with the same xcoordinate as 4
+  //   do they all have the same mark?
+  //     yes? Victory.
+  // spaces with the same ycoordinate as 4
+  //   do they all have the same mark?
+  //     yes? Victory.
+  // spaces with diagonal relation to 4
+  //   do they all have the same mark?
+  //     yes? Victory.
+  //     no? next player turn
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,3 +1,5 @@
+// Player object
+
 describe('Player', function() {
   it("verifies that the player object is built as intended", function() {
     var testPlayer = new Player("Bill", "O");
@@ -5,6 +7,8 @@ describe('Player', function() {
     expect(testPlayer.type).to.equal("O");
   });
 });
+
+// Space object
 
 describe('Space', function() {
   it("verifies that the space object is built as intended", function() {
@@ -14,6 +18,8 @@ describe('Space', function() {
     expect(testSpace.mark).to.equal(null);
   });
 });
+
+// Board object
 
 describe('Board', function() {
   it("verifies that the board object is built as intended", function() {
@@ -27,6 +33,73 @@ describe('Board', function() {
       testBoard.buildBoard();
       expect(testBoard.spaces[2].xCoordinate).to.equal(3);
       expect(testBoard.spaces[2].yCoordinate).to.equal(1);
+    });
+  });
+});
+
+// Game object
+
+describe('Game', function () {
+  it("will create a game with two players and a board", function() {
+    var testPlayerX = new Player("Max", "X");
+    var testPlayerO = new Player("Charlie", "O");
+    var testBoard = new Board();
+    testBoard.buildBoard();
+    var testGame = new Game(testPlayerX, testPlayerO, testBoard);
+    expect(testGame.playerX).to.eql(testPlayerX);
+    expect(testGame.playerX).to.eql(testPlayerX);
+    expect(testGame.board).to.eql(testBoard);
+  });
+
+  describe('victory', function() {
+    it("will determine if a horizontal victory condition has been met", function() {
+      var testPlayerX = new Player("Max", "X");
+      var testPlayerO = new Player("Charlie", "O");
+      var testBoard = new Board();
+      testBoard.buildBoard();
+      var testGame = new Game(testPlayerX, testPlayerO, testBoard);
+      playerOTurn(0);
+      playerXTurn(6);
+      playerOTurn(1);
+      playerXTurn(7);
+      expect(playerOTurn(2)).to.eql("Charlie wins");
+    });
+
+    it("will determine if a vertical victory condition has been met", function() {
+      var testPlayerX = new Player("Max", "X");
+      var testPlayerO = new Player("Charlie", "O");
+      var testBoard = new Board();
+      testBoard.buildBoard();
+      var testGame = new Game(testPlayerX, testPlayerO, testBoard);
+      playerXTurn(0);
+      playerOTurn(2);
+      playerXTurn(3);
+      playerOTurn(5);
+      expect(playerXTurn(6)).to.eql("Max wins");
+    });
+  });
+
+  describe('playerOTurn', function() {
+    it("changes the mark of the corresponding space to O", function() {
+      var testPlayerX = new Player("Max", "X");
+      var testPlayerO = new Player("Charlie", "O");
+      var testBoard = new Board();
+      testBoard.buildBoard();
+      var testGame = new Game(testPlayerX, testPlayerO, testBoard);
+      playerOTurn(6);
+      expect(testGame.testBoard.spaces[6].mark).to.equal("O");
+    });
+  });
+
+  describe('playerXTurn', function() {
+    it("changes the mark of the corresponding space to X", function() {
+      var testPlayerX = new Player("Max", "X");
+      var testPlayerO = new Player("Charlie", "O");
+      var testBoard = new Board();
+      testBoard.buildBoard();
+      var testGame = new Game(testPlayerX, testPlayerO, testBoard);
+      playerXTurn(4);
+      expect(testGame.testBoard.spaces[4].mark).to.equal("X");
     });
   });
 });
