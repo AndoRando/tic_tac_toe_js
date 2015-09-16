b13a5ef1c38ee1771f291644c6ba72c9ef904650
Andrew Brennwald & Jessica Joly <student@epicodus.com>
2015-09-16 14:22:45 -0700

    Implement board object and buildBoard prototype.

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,15 @@ function Space(xCoordinate, yCoordinate, mark) {
   this.yCoordinate = yCoordinate;
   this.mark = mark;
 };
+
+function Board() {
+  this.spaces = [];
+};
+
+Board.prototype.buildBoard = function() {
+  for (var y = 1; y < 4; y++) {
+    for (var x = 1; x < 4; x++) {
+      this.spaces.push(new Space(x, y));
+    };
+  };
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,25 +1,28 @@
 describe('Player', function() {
-  it("verifies that the player object is built as intended",function () {
-    var testPlayer = new Player ("Bill", "O");
+  it("verifies that the player object is built as intended", function() {
+    var testPlayer = new Player("Bill", "O");
     expect(testPlayer.name).to.equal("Bill");
     expect(testPlayer.type).to.equal("O");
   });
 });
 
 describe('Space', function() {
-  it("verifies that the space object is built as intended",function () {
-    var testSpace = new Space (1, 3, null);
+  it("verifies that the space object is built as intended", function() {
+    var testSpace = new Space(1, 3, null);
     expect(testSpace.xCoordinate).to.equal(1);
     expect(testSpace.yCoordinate).to.equal(3);
     expect(testSpace.mark).to.equal(null);
   });
 });
 
-// describe('', function() {
-//   it("", function() {
-//
-//   });
-// });
+describe('Board', function() {
+  it("verifies that the board object is built as intended", function() {
+    var testBoard = new Board();
+    testBoard.buildBoard();
+    expect(testBoard.spaces[2].xCoordinate).to.equal(3);
+    expect(testBoard.spaces[2].yCoordinate).to.equal(1);
+  });
+});
 //
 // describe('', function() {
 //   it("", function() {
