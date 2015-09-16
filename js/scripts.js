dfea93722a3d4f4bd2f1d9e8e8199e28e5964c7f
Andrew Brennwald & Jessica Joly <student@epicodus.com>
2015-09-16 09:54:17 -0700

    Implement Space object constructor.

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,9 @@ function Player(name, type) {
   this.name = name;
   this.type = type;
 };
+
+function Space(xCoordinate, yCoordinate, mark) {
+  this.xCoordinate = xCoordinate;
+  this.yCoordinate = yCoordinate;
+  this.mark = mark;
+};
--- spec/specs.js
+++ spec/specs.js
@@ -5,3 +5,24 @@ describe('Player', function() {
     expect(testPlayer.type).to.equal("O");
   });
 });
+
+describe('Space', function() {
+  it("verifies that the space object is built as intended",function () {
+    var testSpace = new Space (1, 3, null);
+    expect(testSpace.xCoordinate).to.equal(1);
+    expect(testSpace.yCoordinate).to.equal(3);
+    expect(testSpace.mark).to.equal(null);
+  });
+});
+
+// describe('', function() {
+//   it("", function() {
+//
+//   });
+// });
+//
+// describe('', function() {
+//   it("", function() {
+//
+//   });
+// });
